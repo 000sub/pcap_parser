68951675a2f53d87a0db73d3aed5465529cd1d3d
dmong <dt2980_hee@naver.com>
2022-12-11 20:17:56 +0900

    Add files via upload

---
+++ socket_programming/client.c
@@ -0,0 +1,273 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/msg.h> 
+#include <sys/ipc.h> 
+#include <netinet/in.h>
+
+#define MSG_KEY (key_t)1097
+#define PORT_NUM 8202
+
+typedef struct {
+  uint16_t xid;      
+  uint16_t flags;    
+  uint16_t qdcount;  
+  uint16_t ancount; 
+  uint16_t nscount;  
+  uint16_t arcount; 
+} dns_header_t;
+
+typedef struct {
+  char *name;        
+  uint16_t dnstype;  
+  uint16_t dnsclass; 
+} dns_question_t;
+
+typedef struct {
+  uint16_t compression;
+  uint16_t type;
+  uint16_t class;
+  uint32_t ttl;
+  uint16_t length;
+  struct in_addr addr;
+} __attribute__((packed)) dns_record_a_t;
+
+int main(int argc, char** argv){
+    dns_header_t header;
+    dns_question_t question;
+    socklen_t dns_response_len = 0, clilen;
+    unsigned char response[512];
+    struct sockaddr_in sktaddr, dnsaddr, servaddr, cliaddr;
+    char domain_name[50], ip_addr[50];
+    char buf[1024], tokens[1024];
+    int sockfd, dnsfd, listenfd, connfd;
+    int qid;
+    struct msgbuf {
+        long mtype;
+        char mtext[1024];
+    } mydata;
+
+    //create DNS socket
+    if ((dnsfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        printf("DNS socket creating error.\n");
+        exit(1);
+    }
+
+    dnsaddr.sin_family = AF_INET;
+    dnsaddr.sin_addr.s_addr = htonl(0x01010101); //Cloudflare 1.1.1.1
+    dnsaddr.sin_port = htons(53);
+
+    //DNS header set
+    memset(&header, 0x00, sizeof(dns_header_t));
+    header.xid = htons(0x1234);
+    header.flags = htons(0x0100);
+    header.qdcount = htons(1);
+
+    strcpy(domain_name, "2022fcn.ddns.net");
+
+    //DNS question set
+    question.dnstype = htons(1);
+    question.dnsclass = htons(1);
+    question.name = calloc(strlen(domain_name) + 2, sizeof (char));
+    memcpy(question.name +1, domain_name, strlen(domain_name));
+    uint8_t *prev = (uint8_t *) question.name;
+    uint8_t count = 0;
+
+    for (size_t i = 0; i < strlen(domain_name); i++)
+    {
+        if (domain_name[i] == '.')
+        {
+            *prev = count;
+            prev = question.name + i + 1;
+            count = 0;
+        }
+        else
+        count++;
+    }
+    *prev = count;
+
+    //Copy all fields into a single packet 
+    size_t packetlen = sizeof(header) + strlen(domain_name) + 2 + sizeof(question.dnstype) + sizeof(question.dnsclass);
+    uint8_t *packet = calloc(packetlen, sizeof(uint8_t));
+    uint8_t *p = (uint8_t *)packet;
+    memcpy(p, &header, sizeof(header));
+    p += sizeof(header);
+    memcpy(p, question.name, strlen(domain_name) + 1);
+    p += strlen(domain_name) + 2;
+    memcpy(p, &question.dnstype, sizeof(question.dnstype));
+    p += sizeof(question.dnstype);
+    memcpy(p, &question.dnsclass, sizeof(question.dnsclass));
+
+    // send DNS packet
+    if (sendto(dnsfd, packet, packetlen, 0, (struct sockaddr *) &dnsaddr, (socklen_t) sizeof(dnsaddr)) == -1){
+        printf("DNS request error.\n");
+        exit(1);
+    }
+
+    // receive DNS response and close DNS socket
+    memset(&response, 0x00, 512);
+
+    if (recvfrom(dnsfd, response, 512, 0, (struct sockaddr *) &dnsaddr, &dns_response_len) == -1){
+        printf("DNS response error.\n");
+        exit(1);
+    }
+    close(dnsfd);
+
+    dns_header_t *response_header = (dns_header_t *)response;
+    uint8_t *start_of_name = (uint8_t *) (response + sizeof (dns_header_t));
+    uint8_t total = 0;
+    uint8_t *field_length = start_of_name;
+    while (*field_length != 0) {
+        total += *field_length + 1;
+        *field_length = '.';
+        field_length = start_of_name + total;
+    }
+
+    dns_record_a_t *records = (dns_record_a_t *) (field_length + 5);
+    strcpy(ip_addr, inet_ntoa(records[0].addr));
+    
+    //create TCP socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        printf("TCP socket creating error.\n");
+        exit(1);
+    }
+
+    sktaddr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, ip_addr, &sktaddr.sin_addr) != 1){
+        printf("inet_pton call error.\n");
+        exit(1);
+    }
+    sktaddr.sin_port = htons(50000);
+
+    if (connect(sockfd, (struct sockaddr*)&sktaddr, sizeof(sktaddr)) == -1){
+        printf("server connection error.\n");
+        exit(1);
+    }
+
+    //create another TCP socket that receive tokens
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        printf("socket creating error.\n");
+        exit(1);
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT_NUM);
+
+    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
+        printf("bind error.\n");
+        close(listenfd);
+        exit(1);
+    }
+
+    if (listen(listenfd, 1) == -1){
+        printf("listen error.\n");
+        close(listenfd);
+        exit(1);
+    }
+
+
+    //Interact with server
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //1: Type your ID
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //print next instrument in terminal
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //3: Your server IP address
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //print next instrument in terminal
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //5: Your server Port Number
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //print next instrument in terminal
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //7: confirm -> [Y/N]
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //print next instrument in terminal
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //8: confirm -> [Y]
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //print next instrument in terminal
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    //9: If server is concurrent, type OK
+    memset(buf, 0x00, sizeof(buf));
+    read(0, buf, sizeof(buf));
+    write(sockfd, buf, strlen(buf));
+
+    //get tokens
+    sleep(15);
+    memset(tokens, 0x00, sizeof(tokens));
+    clilen = sizeof(cliaddr);
+    connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen);
+    while (recv(connfd, tokens, 1024,0) > 0);
+    tokens[strlen(tokens)-1] = '\n';
+    write(sockfd, tokens, strlen(tokens));
+
+    sleep(3);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+    memset(buf, 0x00, sizeof(buf));
+    read(sockfd, buf, sizeof(buf));
+    printf("%s", buf);
+
+    close(sockfd);
+    close(listenfd);
+    return 0;
+}
---
+++ socket_programming/server.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/msg.h> 
+#include <sys/ipc.h>
+
+#define PORT_NUM 8201
+#define CLI_PORT 8202
+#define LISTEN_QLEN 3
+#define MSG_KEY (key_t)1097
+
+int main(int argc, char** argv){
+    int listenfd, connfd, sockfd;
+    int client_idx = 1;
+    int fd[2];
+    int qid;
+    char buf[1024], tokens[1024];
+    struct sockaddr_in cliaddr, servaddr, sktaddr;
+    socklen_t clilen;
+    pid_t childpid;
+    struct msgbuf {
+        long mtype;
+        char mtext[1024];
+    } mydata;
+
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        printf("socket creating error.\n");
+        exit(1);
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT_NUM);
+
+    if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
+        printf("bind error.\n");
+        close(listenfd);
+        exit(1);
+    }
+
+    if (listen(listenfd, LISTEN_QLEN) == -1){
+        printf("listen error.\n");
+        close(listenfd);
+        exit(1);
+    }
+
+    memset(tokens, 0x00, sizeof(tokens));
+    printf("Server running, wating for connections.\n");
+
+    while (client_idx < 6){
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen);
+
+        printf("%s\n","Received request...");
+
+        //pipe create
+        if (pipe(fd) < 0){
+            printf("pipe error.\n");
+            exit(1);
+        }
+
+        if ( (childpid = fork ()) == 0 ) {//if 0, it’s child process
+
+            printf("Child created for dealing with client %d requests", client_idx);
+
+            //close listening socket
+            close (listenfd);
+
+            memset(buf, 0x00, sizeof(buf));
+
+            while ( recv(connfd, buf, 1024,0) > 0){
+                printf("reading data...\n");
+
+                printf("%s", buf);
+                write(fd[1], buf, sizeof(buf));
+                printf("client %d connection ended.\n", client_idx);
+            }
+
+            close(connfd);
+            exit(0);
+        }
+
+        sleep(2);
+
+        read(fd[0], buf, sizeof(buf));
+        memcpy(tokens+((strlen(buf) - 1)*(client_idx - 1)), buf, strlen(buf)-1);
+        tokens[strlen(tokens)-1] = ',';
+
+        //close socket of the server
+        close(connfd);
+        client_idx++;
+    } 
+
+    //create TCP socket to send tokens
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        printf("TCP socket creating error.\n");
+        exit(1);
+    }
+
+    sktaddr.sin_family = AF_INET;
+    if (inet_pton(AF_INET, "127.0.0.1", &sktaddr.sin_addr) != 1){
+        printf("inet_pton call error.\n");
+        exit(1);
+    }
+    sktaddr.sin_port = htons(CLI_PORT);
+    if (connect(sockfd, (struct sockaddr*)&sktaddr, sizeof(sktaddr)) == -1){
+        printf("server connection error.\n");
+        exit(1);
+    }
+
+    write(sockfd, tokens, sizeof(tokens)-1);
+
+
+    printf("server closed.\n");
+    close(listenfd);
+    close(sockfd);
+
+    return 0;
+}
